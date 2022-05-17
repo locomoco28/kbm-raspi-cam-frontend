9b79c235f795a678d96e959fc32cec98dc145caa
Eren Karahasan <locomoco.dev@gmail.com>
2022-05-17 14:24:59 +0200

    start cam view

--- src/views/CamView.vue
+++ src/views/CamView.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>CAM VIEW</h1>
+  <div class="container">
+    <div class="section">
+      <div class="columns">
+        <div class="column has-text-centered">
+          <h1 class="title">Camera</h1>
+          <br />
+        </div>
+      </div>
+      <div class="container">
+        <figure class="image">
+          <img
+            src="https://media.giphy.com/media/1xVbJBcmaymAvnC5yv/giphy-downsized-large.gif"
+            style="height: 60vh; width: auto; margin: 0 auto"
+          />
+        </figure>
+      </div>
+      <div class="container">
+        <div class="columns">
+          <div class="column is-1 buttons">
+            <button class="button is-primary is-light">Take image</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
