5a962b4f616c423e9c9b0553bb92ffb522e56996
Eren Karahasan <locomoco.dev@gmail.com>
2022-05-17 13:57:13 +0200

    improved gallery

--- src/components/GalleryItem.vue
+++ src/components/GalleryItem.vue
@@ -1,13 +1,35 @@
+<script>
+export default {
+  props: ["img"],
+};
+</script>
+
 <template>
-  <div class="item">
-    <i>
-      <slot name="icon"></slot>
-    </i>
-    <div class="details">
-      <h3>
-        <slot name="heading">GALLERY VIEW</slot>
-      </h3>
-      <slot></slot>
+  <div class="card column is-3">
+    <div class="card-image">
+      <a :href="img.url" :download="encodeURIComponent(img.title) + '.jpg'">
+        <figure class="image is-4by3">
+          <img :src="img.url" :alt="img.title" />
+        </figure>
+      </a>
     </div>
+
+    <div class="card-content">
+      <h3 class="title is-3">{{ img.title }}</h3>
+      <div class="content">
+        <time :datetime="img.timestamp.toISOString()">{{
+          img.timestamp.toLocaleString("de-DE", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })
+        }}</time>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
   </div>
 </template>
--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -30,38 +30,9 @@ const images: Image[] = [
   <div class="columns">
     <div class="column has-text-centered">
       <h1 class="title">Gallery</h1>
-      <br />
     </div>
   </div>
   <div class="row columns is-multiline">
-    <div v-for="(img, i) in images" :key="i" class="card column is-3">
-      <div class="card-image">
-        <figure class="image is-4by3">
-          <img :src="img.url" :alt="img.title" />
-        </figure>
-      </div>
-      <div class="card-header">
-        <p class="card-header-title">
-          {{ img.title }}
-        </p>
-      </div>
-      <div class="card-content">
-        <div class="content">
-          <time :datetime="img.timestamp.toISOString()">{{
-            img.timestamp.toLocaleString("de-DE", {
-              weekday: "long",
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })
-          }}</time>
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Download</a>
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
+    <GalleryItem v-for="(img, i) in images" :key="i" :img="img" />
   </div>
 </template>
